eddd5b4aedadfd0ba39c3765b61164c53cdb6b9a
Matej Parizek <parizmat@fit.cvut.cz>
2022-11-11 11:48:31 +0100

    [HW03:]Time final

--- DU3/time.c
+++ DU3/time.c
@@ -40,8 +40,9 @@ int monthDays(int m,int y){
     return 0;
 }
 
-/*return number of days until start of year
-using @isLeapYear for number of days in leap year*/
+/**
+ * return number of days until start of year
+ * using @isLeapYear for number of days in leap year*/
 long int numberDaysByYear(int y){
     long int days=0;
     for (long int i = 1; i <y ; i++)
@@ -54,8 +55,9 @@ long int numberDaysByYear(int y){
     return days;    
 }
 
-/*return number of days in the every month int the year
-usiing @monthDays returning number of days in the month*/
+/**
+ * return number of days in the every month int the year
+ * using @monthDays returning number of days in the month*/
 int numberDayByMonth(int m, int y){
     int days=0;
     for(int i=1; i<m;i++){
@@ -87,11 +89,16 @@ int nullMinute(int m){
 //control inputs
 int control(int y1, int m1, int d1, int h1, int i1,
             int y2, int m2, int d2, int h2, int i2){ 
-    if(y1>y2|| (m1>m2 && y1==y2) || (m1==m2 && y1==y2 && d1>d2) ||
-        (m1==m2 && y1==y2 && d1==d2 && h1>h2) ||
-        (m1==m2 && y1==y2 && d1==d2 && i1>i2)){
+    if(y1>y2)
         return 0;
-        }
+    if (m1>m2 && y1==y2)
+        return 0;
+    if (m1==m2 && y1==y2 && d1>d2) 
+        return 0;
+    if (m1==m2 && y1==y2 && d1==d2 && h1>h2)
+        return 0;
+    if (m1==m2 && y1==y2 && d1==d2 && h1==h2 && i1>i2)
+        return 0;
     if(y1<1600 || y2<1600 || m1<1 || m1>12 || m2<1 || m2>12 ||
         d1<1 || d2<1 || h1>23 || h1<0 || h2>23 || h2<0 
         || i1<0 || i2<0 || i1>59 || i2>59){
@@ -141,10 +148,6 @@ void calculation (int y1, int m1, int d1, int h1, int i1,
     return;
 }
 
-/**
- * @todo function for sum the bells
- * 
-*/
 int bells ( int y1, int m1, int d1, int h1, int i1,
             int y2, int m2, int d2, int h2, int i2,
             long long int * b1, long long int * b2 ){
@@ -162,81 +165,8 @@ int bells ( int y1, int m1, int d1, int h1, int i1,
 int main ( int argc, char * argv [] )
 {
   long long int b1, b2; 
- assert ( bells ( 2022, 10,  1, 13, 15,
-                   2022, 10,  1, 18, 45, &b1, &b2 ) == 1
-           && b1 == 56
-           && b2 == 20 );
-  assert ( bells ( 2022, 10,  3, 13, 15,
-                   2022, 10,  4, 11, 20, &b1, &b2 ) == 1
-           && b1 == 221
-           && b2 == 143 );
-  assert ( bells ( 2022, 10,  1, 13, 15,
-                   2022, 10,  2, 11, 20, &b1, &b2 ) == 1
-           && b1 == 106
-           && b2 == 65 );
-  assert ( bells ( 2022, 10,  2, 13, 15,
-                   2022, 10,  3, 11, 20, &b1, &b2 ) == 1
-           && b1 == 115
-           && b2 == 78 );
-  assert ( bells ( 2022, 10,  1, 13, 15,
-                   2022, 10,  3, 11, 20, &b1, &b2 ) == 1
-           && b1 == 221
-           && b2 == 143 );
-  assert ( bells ( 2022,  1,  1, 13, 15,
-                   2022, 10,  5, 11, 20, &b1, &b2 ) == 1
-           && b1 == 56861
-           && b2 == 36959 );
-  assert ( bells ( 2019,  1,  1, 13, 15,
-                   2019, 10,  5, 11, 20, &b1, &b2 ) == 1
-           && b1 == 57101
-           && b2 == 37115 );
-  assert ( bells ( 2024,  1,  1, 13, 15,
-                   2024, 10,  5, 11, 20, &b1, &b2 ) == 1
-           && b1 == 57341
-           && b2 == 37271 );
-  assert ( bells ( 1900,  1,  1, 13, 15,
-                   1900, 10,  5, 11, 20, &b1, &b2 ) == 1
-           && b1 == 57101
-           && b2 == 37115 );
-  assert ( bells ( 2022, 10,  1,  0,  0,
-                   2022, 10,  1, 12,  0, &b1, &b2 ) == 1
-           && b1 == 124
-           && b2 == 90 );
-  assert ( bells ( 2022, 10,  1,  0, 15,
-                   2022, 10,  1,  0, 25, &b1, &b2 ) == 1
-           && b1 == 1
-           && b2 == 0 );
-  assert ( bells ( 2022, 10,  1, 12,  0,
-                   2022, 10,  1, 12,  0, &b1, &b2 ) == 1
-           && b1 == 4
-           && b2 == 12 );
-  assert ( bells ( 2022, 11,  1, 12,  0,
-                   2022, 10,  1, 12,  0, &b1, &b2 ) == 0
-           && b1 == 4
-           && b2 == 12 );
-  assert ( bells ( 2022, 10, 32, 12,  0,
-                   2022, 11, 10, 12,  0, &b1, &b2 ) == 0
-           && b1 == 4
-           && b2 == 12 );
-  assert ( bells ( 2100,  2, 29, 12,  0,
-                   2100,  2, 29, 12,  0, &b1, &b2 ) == 0
-           && b1 == 4
-           && b2 == 12 );
-  assert ( bells ( 2000,  2, 29, 12,  0,
-                   2000,  2, 29, 12,  0, &b1, &b2 ) == 1
-           && b1 == 4
-           && b2 == 12 );
-  assert ( bells ( 2004,  2, 29, 12,  0,
-                   2004,  2, 29, 12,  0, &b1, &b2 ) == 1
-           && b1 == 0
-           && b2 == 0 );
-  assert ( bells ( 2000, 12, 28, 0, 0, 2000, 12, 28, 0, 0, &b1, &b2 )==1 
-            && b1==4
-            &&b2==12);
-  assert(bells ( 1929, 1, 8, 13, 19, 2079845, 9, 23, 15, 21, &b1, &b2 )==1 
-            && b1==156125411780
-            && b2==101481517649);
-    
+     bells ( 2087, 1, 18, 22, 55, 2087, 1, 18, 23, 50, &b1, &b2 );
+
   return EXIT_SUCCESS;
 }
 #endif /* __PROGTEST__ */

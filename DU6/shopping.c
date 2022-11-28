1356cfe8f20603907671143abc886d5eed960e26
Matej Parizek <parizmat@fit.cvut.cz>
2022-11-28 20:16:28 +0100

    [HW06:]Shopping.c wrong AI

--- DU6/shopping.c
+++ DU6/shopping.c
@@ -22,17 +22,29 @@ typedef struct Products
     char protuct[201];
     char lowerProduct[201];
     int data;
+    char regalProduct[201];
     struct Products* next;
 }Products_t;
 /**Add product to LinkedList*/
-void newProduct(Products_t** products,char *product)
+void newProduct(Products_t** products,char *product, const char *regalProd)
 {
     Products_t* temp=(Products_t*)malloc(sizeof(Products_t));
+    Products_t* last=*products;
     strcpy(temp->protuct,product);
+    strcpy(temp->regalProduct,regalProd);
     strcpy(temp->lowerProduct,lowerString(product));
-    temp->next=*products;
-    
-    *products=temp;
+    temp->next=NULL;
+    if(*products==NULL)
+    {
+        *products=temp;
+        return;
+    }
+    while (last->next!=NULL)
+    {
+        last=last->next;
+    }
+    last->next=temp;
+    return;
 }
 /**Clear LINKED LIST*/
 void clearLinkedList(Products_t* products)
@@ -96,20 +108,20 @@ void add(Quote_t* quote, int regal)
     return;
 }
 
-void addSort(Quote_t* quote, int regal, char* product)
+void addSort(Quote_t* quote, int regal, char* product,char *name)
 {
     Node_t* node=newNode(regal);
     //prazdna fronta
     if(!quote->last)
     {
         quote->first=quote->last=node;
-        newProduct(&quote->first->products,product);
+        newProduct(&quote->first->products,product,name);
         return;
     }
     //pokud uz regal na zacatku existuje tak nic neprobehne
     if(quote->first->regals==regal)
     {    
-        newProduct(&quote->first->products,product);
+        newProduct(&quote->first->products,product,name);
         free(node);
         return;
     }
@@ -118,7 +130,7 @@ void addSort(Quote_t* quote, int regal, char* product)
     {
         node->next=quote->first;
         quote->first=node;
-        newProduct(&quote->first->products,product);
+        newProduct(&quote->first->products,product,name);
         return;
     }
 
@@ -133,14 +145,14 @@ void addSort(Quote_t* quote, int regal, char* product)
         //pokud je u prostred vynech
         if (temp.first->next->regals==regal)
         {
-            newProduct(&temp.first->next->products,product);
+            newProduct(&temp.first->next->products,product,name);
             free(node);
             return;
         }
         node->next=temp.first->next;
     }
     temp.first->next=node;
-    newProduct(&temp.first->next->products,product);
+    newProduct(&temp.first->next->products,product,name);
     return;
 }
 
@@ -169,7 +181,10 @@ void printfLinkedList(Products_t* products, int regal,int* position)
 {
     while (products!=NULL)
     {
-        printf("%d. %s -> #%d\n",*position,products->protuct,regal);
+        if(regal!=-1)
+            printf(" %d. %s -> #%d %s\n",*position,products->protuct,regal,products->regalProduct);
+        else
+            printf(" %d. %s -> %s\n",*position,products->protuct,products->regalProduct);
         Products_t *temp=products->next;
         free(products);
         *position+=1;
@@ -177,7 +192,7 @@ void printfLinkedList(Products_t* products, int regal,int* position)
     }
     
 }
-void printfQuote(Quote_t * quote)
+void printfQuote(Quote_t * quote, Products_t* noList)
 {
     if(quote->first==NULL)
     {
@@ -193,40 +208,58 @@ void printfQuote(Quote_t * quote)
         quote->first=temp;
     }
     free(quote);
+    printfLinkedList(noList,-1,&position);
 }
 
 
 /**************searching but not ideal worst idea*********************/
-int searchLinkedList(Products_t products,const char *name)
+char *searchLinkedList(Products_t products,const char *name)
 {
     Products_t *temp=&products;
     while (temp!=NULL)
-    {
+    {   
         if(strstr(temp->lowerProduct,name)!=NULL)
         {
-            printf("%s\n",temp->lowerProduct);
-            return 1;
+            return temp->protuct;
         }
         temp=temp->next;
     }
-    return 0;
+    return NULL;
 }
-int searchQuote(Quote_t quote, char *name)
+int searchQuote(Quote_t* list, Quote_t quote, char *name)
 {
-    int count=0;
-    char lowerName[201];
-    strcpy(lowerName,name);
-    lowerString(lowerName);
+    char temp[201];temp[0]='\0';
+    int regal=0;  int minRegal=__INT32_MAX__;
+    char help[201]; strcpy(help,name);
+    int lenName=(int)strlen(name);
+    lowerString(help);
     while (quote.first!=NULL)
     {
-        if(searchLinkedList(*quote.first->products,lowerName))
+        int lenMax=0;
+        char *buffer=searchLinkedList(*quote.first->products, help);
+        if(buffer!=NULL)
         {
-            return count;
+            int len=(int)strlen(buffer);
+            if(lenMax<len)
+            {                
+                strcpy(temp,buffer);
+                regal=quote.first->regals;
+                lenMax=len;
+                if(lenName==lenMax)
+                {
+                    break;
+                }
+            }
         }
         quote.first=quote.first->next;
-        count++;
+    }
+    if(temp[0]!='\0')
+    {
+        addSort(list,regal,name,temp);
+        return 1;
     }
     return -1;
+       
 }
 
 
@@ -255,7 +288,7 @@ int readRegals(Quote_t** quote, FILE *file){
         int p = strcspn(buffer,enter);
         if(p!=0)
             buffer[p]='\0';
-        newProduct(&(*quote)->last->products,buffer);
+        newProduct(&(*quote)->last->products,buffer,buffer);
     }    
     return 0;
 }
@@ -265,24 +298,31 @@ void commodity(Quote_t* quote,FILE *file)
 {
     char buffer[201];
     Quote_t* list=creatQuote();
-    
+    Products_t *notList=NULL;
+    printf("Optimalizovany seznam:\n");
     while(fgets(buffer,200,file))
     {
+        char enter[2]; enter[0]='\n';enter[1]='\0';
+        int p = strcspn(buffer,enter);
+        if(p!=0)
+            buffer[p]='\0';
 
         if(buffer[0]=='\n')
         {
-            printfQuote(list);
-            printf("-----------------\n ");
+            printfQuote(list,notList);
             commodity(quote,file);
             return;
         } 
+
         //pozistion in regal and product in regal;
-        int s=searchQuote(*quote,buffer);
+        int s=searchQuote(list, *quote,buffer);
         if(s==-1)
+        {
+            newProduct(&notList,buffer,"N/A");
             continue;
-        addSort(list,s,buffer);
+        }
     }
-    printfQuote(list);
+    printfQuote(list,notList);
     return;
 }
 
@@ -294,18 +334,16 @@ void commodity(Quote_t* quote,FILE *file)
 int main(int argc,char *argv[])
 {
     char * filename1=argv[1];
-    char * filename2=argv[2];
 
-    if(filename1==NULL || filename2==NULL)
+    if(filename1==NULL)
      {
         fprintf(stderr,"Nezadan nazev souboru!");
         return 101;
     }
 
     FILE *file1 = fopen(filename1, "r");
-    FILE *file2 = fopen(filename2, "w");
 
-    if(!file1 || !file2)
+    if(!file1)
     {
         fprintf(stderr,"Neotevrel se soubor");
         return 100;
@@ -314,14 +352,10 @@ int main(int argc,char *argv[])
     Quote_t* quote=creatQuote();
     //read file
     readRegals(&quote,file1);
-    //commodity(quote,file1);
-    //printf("%s\n",quote->first->products->protuct);
-
+    commodity(quote,file1);
     fclose(file1);
-    fclose(file2);
 
-    printfQuote(quote);
     //free all memory
-    //clearQuote(quote);
+    clearQuote(quote);
     return 0;
 }
